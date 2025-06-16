529a5a0386e4321e4c9373f3a30dc44f1abc3767
Ibrahim <78224716+ei-bro@users.noreply.github.com>
2025-06-17 00:42:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='header'>My React App</h1>
+      <div className='card'>
+        <h2 className='card-title'>Card Title</h2>
+        <p className='card-description'>
+          This is a simple card with a title and description.
+        </p>
+        <button className='button'>Click Me</button>
+        <input type='text' className='input' placeholder='Enter text' />
+      </div>
+      <div className='card'>
+        <h2 className='card-title'>Card Title</h2>
+        <p className='card-description'>
+          This is a simple card with a title and description.
+        </p>
+        <button className='button'>Click Me</button>
+        <input type='text' className='input' placeholder='Enter text' />
+      </div>
+    </div>
+  );
+}
+export default App;
